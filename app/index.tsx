02ac50c3be10b79828ee564734de583d2ef8270c
Kathryn Tan <144634477+Ktan61@users.noreply.github.com>
2024-09-08 18:27:20 -0700

    Initial commit for mobileapp

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{
+        fontSize:50
+      }}>Welcome to my app!</Text>
+    </View>
+  );
+}
